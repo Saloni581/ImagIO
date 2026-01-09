4dd4a7286c0dcb771cb2bf57b0979d9549e4dfa8
saloni <salonilathwariya581@gmail.com>
2026-01-10 02:48:07 +0530

    create a dashboard with a simple form to generate image and also show the generated image.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    images: {
+        domains: [
+            'pub-static.aiease.ai',
+        ]
+    }
 };
 
 export default nextConfig;
